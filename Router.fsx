5e835c85b7f1c0e7d2733e5750f864f49109e3cd
Magnus Dalsgaard Larsen <daml@itu.dk>
2023-04-02 01:49:09 +0200

    changed to use bytes, handle disconnects

--- Router.fsx
+++ Router.fsx
@@ -3,8 +3,9 @@
     communications to other ports based on an initial identification message.
 *)
 
-open System.Net.Sockets
+open System
 open System.Net
+open System.Net.Sockets
 open System.IO
 
 let ADDRESS = IPAddress.Any
@@ -12,10 +13,19 @@ let PORT = 5000
 
 let INNER_ADDRESS = "localhost"
 
+let INNER_PORT_TIMEOUT = 10 * 1000
+let BLOCK_SIZE = 1024
+
 let listener = new TcpListener(ADDRESS, PORT)
 listener.Start()
 
 printfn "Listener started"
+
+let relayBytes (fromStream: NetworkStream) (toStream: NetworkStream) =
+    let buffer = Array.zeroCreate BLOCK_SIZE
+    let dataSize = fromStream.Read(buffer, 0, BLOCK_SIZE)
+    toStream.Write(buffer, 0, dataSize)
+    sprintf "%A" buffer
 
 while true do
     let client = listener.AcceptTcpClient()
@@ -23,36 +33,65 @@ while true do
     async {
         (* Setup client stream *)
         let stream = client.GetStream()
-        let reader = new StreamReader(stream)
-        let writer = new StreamWriter(stream)
+        use reader = new StreamReader(stream, leaveOpen=true) // Used to recieve inner port
 
         (* Setup inner client for server communicaion *)
-        let innerPort = reader.ReadLine() |> int
-        let innerClient = new TcpClient()
-        innerClient.Connect(INNER_ADDRESS, innerPort)
-        printfn "Client(%d) mapped to Server(%d)" (client.GetHashCode()) innerPort
-        
-        (* Setup inner client stream *)
-        let innerStream = innerClient.GetStream()
-        let innerReader = new StreamReader(innerStream)
-        let innerWriter = new StreamWriter(innerStream)
-        
-        async { // Relay client messages to server
-            while true do
-                let msg = reader.ReadLine()
-                printfn "Client(%d) -> Server(%d): %s" (client.GetHashCode()) innerPort msg
-                innerWriter.WriteLine(msg)
-                innerWriter.Flush()
-        }
-        |> Async.Start
+        let innerPort = 
+            try
+                stream.ReadTimeout <- INNER_PORT_TIMEOUT
+                let innerPort = Some (reader.ReadLine() |> int)
+                stream.ReadTimeout <- Threading.Timeout.Infinite
+                innerPort
+            with
+            | :? FormatException
+            | :? IOException -> None
+        match innerPort with
+        | None -> printfn "Client(%d) lost before providing inner port" (client.GetHashCode())
+                  client.Close()
+        | Some innerPort ->
+            let innerClient = new TcpClient()
+            if // if we successfully connect innerClient to the server
+                try
+                    innerClient.Connect(INNER_ADDRESS, innerPort)
+                    true
+                with
+                | :? SocketException -> printfn "Client(%d) failed to connect to Server(%d) (server doesn probably not exist)" (innerClient.GetHashCode()) innerPort
+                                        client.Close()
+                                        innerClient.Close()
+                                        false
+            then
+                reader.Dispose()
+                printfn "Client(%d) mapped to Server(%d)" (client.GetHashCode()) innerPort
+                
+                (* Setup inner client stream *)
+                let innerStream = innerClient.GetStream()
+                
+                let mutable doRelay = true
+                let stop() =
+                    doRelay <- false
+                    client.Close()
+                    innerClient.Close()
 
-        async { // Relay server messages to client
-            while true do
-                let msg = innerReader.ReadLine()
-                printfn "Server(%d) -> Client(%d): %s" innerPort (client.GetHashCode()) msg
-                writer.WriteLine(msg)
-                writer.Flush()
-        }
-        |> Async.Start
+                async { // Relay client messages to server
+                    while doRelay do
+                        try
+                            let msg = relayBytes stream innerStream
+                            printfn "Client(%d) -> Server(%d): %s" (client.GetHashCode()) innerPort msg
+                        with
+                        | :? IOException -> printfn "Client(%d) lost" (client.GetHashCode())
+                                            stop()
+                }
+                |> Async.Start
+
+                async { // Relay server messages to client
+                    while doRelay do
+                        try
+                            let msg = relayBytes innerStream stream
+                            printfn "Server(%d) -> Client(%d): %s" innerPort (client.GetHashCode()) msg
+                        with
+                        | :? IOException -> printfn "Server(%d) lost" innerPort
+                                            stop()
+                }
+                |> Async.Start
     }
     |> Async.Start
